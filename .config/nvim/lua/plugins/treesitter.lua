6870f785dc135b79584c7acd9a1fcf026df8cded
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-11-23 08:49:45 -0500

    fix: treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "typescript" },
+          ensure_installed = { "lua", "vim", "query", "javascript", "html", "java", "typescript" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
