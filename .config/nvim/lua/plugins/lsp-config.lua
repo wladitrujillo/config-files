dd00917b8ab82a546e355a35ad1af350f1ac667b
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-12-09 09:27:50 -0500

    add: html cssls lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls"}
+                ensure_installed = { "lua_ls", "ts_ls", "html", "cssls" }
             })
         end
     },
@@ -19,6 +19,8 @@ return {
            local lspconfig = require('lspconfig')
            lspconfig.lua_ls.setup({})
            lspconfig.ts_ls.setup({})
+           lspconfig.html.setup({})
+           lspconfig.cssls.setup({})
            vim.keymap.set('n', 'K', vim.lsp.buf.hover)
            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
