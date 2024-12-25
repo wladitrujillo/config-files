366405c7daf4b7cc6c8cbcefb8415d6e729cd14b
Nelson Trujillo <nelson.trujillo@cobistopaz.com>
2024-12-25 09:06:06 -0500

    fix: lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "html", "cssls" }
+                ensure_installed = { "angularls", "lua_ls", "ts_ls", "html", "cssls", "sqlls", "yamlls" }
             })
         end
     },
@@ -17,10 +17,19 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
            local lspconfig = require('lspconfig')
+           local util = require('lspconfig.util')
            lspconfig.lua_ls.setup({})
            lspconfig.ts_ls.setup({})
            lspconfig.html.setup({})
            lspconfig.cssls.setup({})
+           lspconfig.sqlls.setup({
+                cmd = { 'sql-language-server', 'up', '--method', 'stdio' },
+                filetypes = { 'sql', 'mysql', 'sp' },
+                root_dir = util.root_pattern '.sqllsrc.json',
+                settings = {},
+           })
+           lspconfig.yamlls.setup({})
+           lspconfig.angularls.setup({})
            vim.keymap.set('n', 'K', vim.lsp.buf.hover)
            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
